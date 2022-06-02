434558d7f927398bcb1c72c5b021b47add4e604f
Richard Mbingi <64406225+Richard-Mbingi@users.noreply.github.com>
2022-06-02 14:26:56 +0300

    Custom fonts added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-mono bg-gradient-to-r from-secondary to-primary">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#191919",
+      secondary: "#F3F3F3",
+      gdYellow: "#F3DD30",
+      gdRed: "#DF314D",
+      gdIndigo: "#6F6FB2",
+      gdBlue: "#57A9C2",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", ],
+        mono: ["Inconsolata", "sans-serif"],
+        logo: ["Inconsolata"],
+      },
+    },
   },
+
+  variants: {
+    extend: {
+      fontFamily: ["hover", "focus"],
+    },
+  },
+
   plugins: [],
+  corePlugins: {
+    fontFamily: true,
+  },
 };
